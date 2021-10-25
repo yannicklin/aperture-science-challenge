12c5519fab5ad4f3326ef7a84109b48ac406402b
Sam Wolski <sam.wolski@intellihr.com>
2021-10-25 12:06:47 +1000

    Swap from apache to nginx, install lighthouse/graphql

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+// $app->register(\Nuwave\Lighthouse\LighthouseServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
