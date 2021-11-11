a52958d8e91cf92826af08dbe1809d3c6ea5b3f5
Sam Wolski <sam.wolski@intellihr.com>
2021-11-11 12:48:12 +1000

    Add some more graphql tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,74 +1,13 @@
-let LOCAL_STORAGE_MEMORY = {}
-
 describe('Aperture Science Enhancement Center Cypress Tests', () => {
-  Cypress.Cookies.debug(true);
-  Cypress.Cookies.defaults({
-    preserve: (cookie) => {
-      return /^(laravel|XSRF).+/.test(cookie.name)
-    }
-  })
-
   it('finds the home page', function () {
     cy.visit('http://host.docker.internal:3000')
     cy.get('h1');
   })
-  
-  // it('can log in and see a list of subjects', function () {
-  //   let cookies = null
-  //   let token = ''
 
-  //   cy.visit('http://host.docker.internal:3000')
-  //   cy.get('#email').type(Cypress.env('email'));
-  //   cy.get('#password').type(Cypress.env('password'));
-
-  //   cy.intercept('http://webserver/sanctum/csrf-cookie', (req) => {
-  //     req.on('response', (res) => {
-  //       cookies = res.headers["set-cookie"]
-  //     })
-  //   }).as('token')
-
-  //   cy.intercept('POST', 'http://webserver/login', (req) => {
-  //     for (const cookie of cookies) {
-  //       const name = cookie.split(';')[0].split("=")[0]
-  //       const val = cookie.split(';')[0].split("=")[1]
-
-  //       if (name === 'XSRF-TOKEN') {
-  //         token = decodeURIComponent(val)
-  //         req.headers['X-XSRF-TOKEN'] = token
-  //       }
-  //     }
-  //     req.headers['Cookie'] = cookies.join(';')
-
-  //     req.on('response', (res) => {
-  //       cookies = res.headers["set-cookie"]
-  //     })
-  //   }).as('login')
-
-  //   cy.get('#submit').click()
-
-  //   cy.wait('@token')
-  //   cy.wait('@login')
-  //     .its('request.headers')
-  //     .should('have.property', 'X-XSRF-TOKEN')
-  //     .then(() => {
-  //       for (const cookie of cookies) {
-  //         const name = cookie.split(';')[0].split("=")[0]
-  //         const val = cookie.split(';')[0].split("=")[1]
-  //         cy.setCookie(name, val)
-  //       }
-  //     })
-
-  //   cy.intercept('POST', 'http://webserver/graphql', (req) => {
-  //     req.headers['Cookie'] = cookies.join(';')
-  //   }).as('graphql')
-
-  //   cy.location('pathname').should('eq', '/subjects')
-  //   cy.get('h1').contains('Testing Subjects');
-  //   cy.get('div[data-testid="skeleton"]');
-
-  //   cy.wait('@graphql')
-  //   cy.get('table[data-testid="subjects-table"]');
-  // })
+  it('cant find the subjects page without logging in', function () {
+    cy.visit('http://host.docker.internal:3000/subjects')
+    cy.location('pathname').should('eq', '/')
+  })
 
   it('can log in and see a list of subjects', function () {
     cy.visit('http://host.docker.internal:3000')
@@ -82,12 +21,54 @@ describe('Aperture Science Enhancement Center Cypress Tests', () => {
 
     cy.intercept('POST', 'http://host.docker.internal/graphql').as('graphql')
 
-    cy.location('pathname').should('eq', '/subjects')
     cy.get('h1').contains('Testing Subjects');
     cy.get('div[data-testid="skeleton"]');
 
     cy.wait('@graphql')
     cy.get('table[data-testid="subjects-table"]');
   });
+
+  it('can request subjects', function() {
+    const query = `
+      query {
+        subjects {
+          id
+          name
+          test_chamber
+          date_of_birth
+          score
+          alive
+          created_at
+        }
+      }
+    `
+    cy.request({
+      method: 'POST', 
+      url: 'http://host.docker.internal/graphql', 
+      body: { query }
+    }).then((res) => {
+      cy.log(res.body)
+      expect(res.body, 'response body').property('data').property('subjects').to.have.lengthOf(10)
+    })
+  });
+
+  it('cant request users without authentication', function() {
+    const query = `
+      query {
+        users {
+          id
+          name
+        }
+      }
+    `
+    cy.request({
+      method: 'POST', 
+      url: 'http://host.docker.internal/graphql', 
+      body: { query }
+    }).then((res) => {
+      cy.log(res.body)
+      expect(res.body, 'response body').property('errors').to.exist
+    })
+  })
 
 })
